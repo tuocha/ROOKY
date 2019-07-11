28fc0d89eac5601dfafbaed16f6870299c8b9e71
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-07-11 14:30:18 -0400

    news article working code

--- assets/javascript/NYT.js
+++ assets/javascript/NYT.js
@@ -1,4 +1,5 @@
-var authKey = "MnemG5WzI6XTAXqn55SrKwgKJXzcahEG";
+var authKey = "PunvAMm65w6cXjYDAbi01tl4hhmrIb59";
+
 
 
 
@@ -7,37 +8,41 @@ var authKey = "MnemG5WzI6XTAXqn55SrKwgKJXzcahEG";
 var articleCounter = 0;
 
 
-function NYT() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=MnemG5WzI6XTAXqn55SrKwgKJXzcahEG";  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    
-    // Grab text the user typed into the search input, add to the queryParams object
-    
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-      }).then(function(response) {
-          console.log(response)
-          $('#article').empty();
+$("#submit").on("click",function(){
 
-          for (var i = 0; i <= response.results.length; i++) {
-            // create new row
-            var newRow = $('<tr>');
-            newRow.attr('data-ID', response.results[i].id);
-            newRow.append('<td>' + response.results[i].title + '</td>');
-            newRow.append('<td><a href="'+ response.results[i].url + ' target="_blank"><button type="button" class="btn btn-info">Read More</button></td>');            
+  var userEntry = $("#searchInput").val();
+     // queryURL is the url we'll use to query the API
+     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ userEntry + "&news_desk:('technology')&api-key=PunvAMm65w6cXjYDAbi01tl4hhmrIb59";  
+     // Begin building an object to contain our API call's query parameters
+     // Set the API key
+     
+     // Grab text the user typed into the search input, add to the queryParams object
+     
+     $.ajax({
+         url: queryURL,
+         method: 'GET'
+       }).then(function(response) {
+           console.log(response.response);
+           $('#articles').empty();
+ 
+           for (var i = 0; i < response.response.docs.length; i++) {
+             // create new row
+             var newRow = $('<tr>');
+             newRow.attr('data-ID', response.response.docs[i]._id);
+             newRow.append('<td>' + response.response.docs[i].headline.main + '</td>');
+             newRow.append('<td><a href="'+ response.response.docs[i].web_url + ' target="_blank"><button type="button" class="btn btn-info">Read More</button></td>');            
+            
+          
+     
+     
+             // append it onto the search-body tably
+             $('#articles').append(newRow);
+             $('#articles').append("<tr class='spacer'></tr>");
+       };
+     })
+
+});
+
     
-         
-    
-    
-            // append it onto the search-body tably
-            $('#article').append(newRow);
-            $('#article').append("<tr class='spacer'></tr>");
-      };
-    })
-}
-    NYT();
     
   
